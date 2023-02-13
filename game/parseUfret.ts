2fdea76a36e66ba9953f93c576cf282f87af5249
eudpna <eudpna@gmail.com>
2023-02-13 18:14:31 +0900

    remove unused codes

--- game/parseUfret.ts
+++ game/parseUfret.ts
@@ -6,11 +6,7 @@ export function parseUfret(html: string) {
     const m = re.exec(html)
     const title = m![1]
     const artist = m![2]
-    const mts = [
-        `{title: ${title}}`,
-        `{artist: ${artist}}`,
-    ]
-    // return mts.join('\n') + '\n' + score
+  
     return {
         score,
         title,
@@ -24,9 +20,9 @@ function parseUfretScore(html: string) {
     const str = m![1]
     const obj = JSON.parse(str)
     const obj1 = obj.map(line => {
-        return line.replaceAll('♭', 'b')
+        return line
+            .replaceAll('♭', 'b')
             .replaceAll('♯', '#')
-
             .replaceAll('\r', '\n')
     })
     return obj1.join('')
